8cb1213bb45d50e3fd7f098fa04425f2259eecec
Lukas Schneider <ls@schneider-a.com>
2018-06-20 11:34:34 +0200

    4th argument for on/off gui

--- Examples/ROS/ORB_SLAM2/src/ros_mono.cc
+++ Examples/ROS/ORB_SLAM2/src/ros_mono.cc
@@ -48,15 +48,18 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "Mono");
     ros::start();
 
-    if(argc != 3)
+    if(argc < 3)
     {
         cerr << endl << "Usage: rosrun ORB_SLAM2 Mono path_to_vocabulary path_to_settings" << endl;        
         ros::shutdown();
         return 1;
     }    
+	bool useViewer =  true;
+    if (argc == 4)
+    	useViewer = false;
 
     // Create SLAM system. It initializes all system threads and gets ready to process frames.
-    ORB_SLAM2::System SLAM(argv[1],argv[2],ORB_SLAM2::System::MONOCULAR,true, true);
+    ORB_SLAM2::System SLAM(argv[1],argv[2],ORB_SLAM2::System::MONOCULAR, useViewer, true);
 
     ImageGrabber igb(&SLAM);
 
